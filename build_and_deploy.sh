4cf7f455a30a1bf0fee10e252cacc14688f099d3
Dimitri Belopopsky <dimitri@belopopsky.com>
2021-01-29 21:06:08 +0100

    Publishing to gh-pages (build_and_deploy.sh)

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -19,7 +19,7 @@ echo "Removing existing files"
 rm -rf build/*
 
 echo "Generating site"
-godot --export "HTML5" build/game.html
+godot --export "HTML5" build/index.html
 
 echo "Updating gh-pages branch"
 cd build && git add --all && git commit -m "Publishing to gh-pages (build_and_deploy.sh)"
