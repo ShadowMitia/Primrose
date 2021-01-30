a3f29702ef17c99b336eed6ec2ce1d5bc979dfaf
Dimitri Belopopsky <dimitri@belopopsky.com>
2021-01-30 01:46:00 +0100

    Quickfix picking up object still visible

--- Scripts/PickableZone.gd
+++ Scripts/PickableZone.gd
@@ -3,7 +3,10 @@ extends Area2D
 func _on_Area2D_body_entered(body):
 	#if (body.has_method("add_object")):
 	
-	if (body is PrimRose):	
-		var old_parent = get_parent().get_parent()
-		old_parent.remove_child(get_parent())
-		body.add_child(get_parent())
+	if (body is PrimRose):
+		var obj = get_parent()
+		obj.hide()
+		var old_parent = obj.get_parent()
+		old_parent.remove_child(obj)
+		body.add_child(obj)
+		obj.position = Vector2(0.0, 0.0)
